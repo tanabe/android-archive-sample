9c9066e23c62a7b133d842601c9a79030e9010ed
Hideaki Tanabe <tanablog@gmail.com>
2019-03-03 22:37:56 +0900

    update

--- safetynetapimodule/src/main/java/com/kaihatsubu/android/safetynetapimodule/SafetyNetAPIUtil.java
+++ safetynetapimodule/src/main/java/com/kaihatsubu/android/safetynetapimodule/SafetyNetAPIUtil.java
@@ -2,6 +2,16 @@ package com.kaihatsubu.android.safetynetapimodule;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
+import com.google.android.gms.safetynet.SafetyNetClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class SafetyNetAPIUtil {
     static public void showMessage(Context context, String title, String message) {
@@ -11,5 +21,16 @@ public class SafetyNetAPIUtil {
                 .setPositiveButton("OK", null)
                 .setNegativeButton("Cancel", null)
                 .show();
+    }
+
+    static public boolean isGooglePlayServicesAvailable(Context context) {
+        GoogleApiAvailability instance = GoogleApiAvailability.getInstance();
+        int result = instance.isGooglePlayServicesAvailable(context);
+        Log.d("tag_name","result is: " + result);
+        if (result == ConnectionResult.SUCCESS) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
